9afb7c2319d18134ccaeea2d81011ea2e9cd2bde
Garrick Aden-Buie <garrick@adenbuie.com>
2021-06-26 15:40:13 -0400

    Initial starter files

---
+++ 00_setup.R
@@ -0,0 +1,20 @@
+# ┌ #useR!2021 ───────────────────────────┐
+# │                                       │
+# │            Professional,              │
+# │            Polished,                  │
+# |            Presentable                │
+# │                                       │
+# └───────────────── @grrrck & @spcanelon ┘
+#
+# https://presentable-user2021.netlify.app/
+
+
+# Core Packages -----------------------------------------------------------
+install.packages(c("xaringan", "metathis"))
+install.packages("xaringanthemer", dependencies = TRUE)
+
+
+# Extra Packages ----------------------------------------------------------
+install.packages("remotes")
+remotes::install_github("gadenbuie/xaringanExtra")
+remotes::install_github("jhelvy/xaringanBuilder")
