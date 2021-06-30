ec237cabf6740e9c2d3fc8e281cd2d45656e47a4
Garrick Aden-Buie <garrick@adenbuie.com>
2021-06-30 08:16:55 -0400

    Drop xaringanBuilder, add fontawesome (#3)

--- 00_setup.R
+++ 00_setup.R
@@ -17,4 +17,5 @@ install.packages("xaringanthemer", dependencies = TRUE)
 # Extra Packages ----------------------------------------------------------
 install.packages("remotes")
 remotes::install_github("gadenbuie/xaringanExtra")
-remotes::install_github("jhelvy/xaringanBuilder")
+
+install.packages("fontawesome")
